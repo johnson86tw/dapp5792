5cda6c413cf89294e5d8fe8ceb439473ebab1edc
Johnson Chen <chnejohnson@gmail.com>
2025-09-10 10:42:07 +0800

    feat: github pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home.vue'
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
+	history: createWebHistory('/dapp5792/'),
 	routes: [
 		{
 			path: '/',
